b2f504d2e92292f6383480abe97d87b2125428cf
Robert Carr <robertcarrcisd@gmail.com>
2022-09-17 18:04:23 -0500

    Added player pixel art to the player
    The player is now a pixelated pirate instead of a red rectangle.

--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 import pygame
+from support import import_folder
 
 class Player(pygame.sprite.Sprite):
     # @brief A function for initializing the Player
     def __init__(self, position):
         super().__init__()
-        self.image = pygame.Surface((32, 64))
-        self.image.fill("red")
+        self.import_character_assets() # Importing the animation images
+        self.frame_index     = 0
+        self.animation_speed = 0.15
+        self.image = self.animations["idle"][self.frame_index]
         self.rect = self.image.get_rect(topleft = position)
         
         # Player Movement
@@ -13,6 +16,17 @@ class Player(pygame.sprite.Sprite):
         self.movement_multiplier_x  = 6     # Movement multiplier that multiplies the movement in update(self)
         self.gravity                = 0.8
         self.jump_speed             = -13   # Remember that to move up in the y-direction, it needs to be negative
+
+    # @brief importing all of the character animation frames
+    def import_character_assets(self):
+        character_path = "graphics/character/"
+        # Creating a dictionary of animations: we have a folder with folders named idle, run, jump and fall
+        self.animations = {"idle":[], "run":[], "jump":[], "fall":[]}
+
+        # Using our dictionary we can just access the full character path to those files
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
 
     # @brief A function for getting player input
     def get_input(self):
@@ -31,7 +45,6 @@ class Player(pygame.sprite.Sprite):
 
         if keys[pygame.K_UP]:
             self.jump()
-        
 
     # @brief A function for applying gravity to the player
     def apply_gravity(self):
