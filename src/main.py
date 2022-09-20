0e5c0992cad614df0b6513da513b6bf951482831
Robert Carr <robertcarrcisd@gmail.com>
2022-09-20 12:42:57 -0500

    Now correctly getting level data
    The level content is now loaded in successfully. But it is just text.

--- src/main.py
+++ src/main.py
@@ -6,14 +6,16 @@ import pygame
 import sys                          # Allows for sys.exit()
 from settings   import *            # Allows for us to access variables in settings.py
 from overworld  import Overworld
+from level      import Level
 
 class Game:
     def __init__(self):
         self.max_level = 2 # Remember, level number starts at 0, then goes 1, 2, 3, etc. So level 3 is 4 levels
         self.overworld = Overworld(0, self.max_level, screen) # Arguments - start_level, max_level, surface)
+        self.level = Level(1, screen)
 
     def run(self):
-        self.overworld.run()
+        self.level.run()
 
 pygame.init()
 pygame.display.set_caption("Platformer") 
